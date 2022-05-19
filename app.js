93f657de8d498e88cb54bbbe3f5fff268aa16e25
Thiagoocosta86 <74249031+Thiagoocosta86@users.noreply.github.com>
2022-05-19 12:04:25 +0100

    updating js
    variable unitcost and day cost included

--- app.js
+++ app.js
@@ -2,18 +2,33 @@
 
 const unitInput = document.getElementById('unit-input');
 const daysInput = document.getElementById('bp-input');
-//const vatInput = document.getElementById('vat-input');
+
 
 // buttons
 
-const successButton = document.getElementById('btn-calculate');
-//const resetButton = document.getElementById('btn-reset');
+const calcButton = document.getElementById('btn-calculate');
+
 
 // Results
 
 const subCostInput = document.getElementById('dynamic_sub_cost');
 const finalCostInput = document.getElementById('dynamic_total_cost');
 const message = document.getElementById('cost_text')
+
+
+function handleEvent() {
+    if(Number(unitInput.value) && Number(daysInput.value)) {
+        calculateTotalCost()
+    } else {
+        alert('Give some valid inputs first and try again!');
+        
+    }
+
+}
+
+
+calcButton.addEventListener('click', handleEvent);
+
 
 function calculateTotalCost() {
 
@@ -33,50 +48,12 @@ function calculateTotalCost() {
     
     finalCostInput.textContent = total.toFixed(2)
 
-    let msg = showMessage(total);
-
-    message.textContent = msg
-
+   
 }
 
 
 
-function showMessage(cost) {
-    if (cost < 100) {
-        return "Cost is Cheap";
-    } else if (cost >= 100 && cost < 500) {
-        return "Cost is medium";
-    }else if (cost >= 500 && cost < 1000) {
-        return "Cost is high";
-    } else if (cost >= 1000 && cost <= 5000) {
-        return "Cost is expensive";
-    } else {
-        return "cost is super expensive";
-    }
-}
-
-
-//function reset() {
-//    unitInput.value = ""
-//    daysInput.value = ""
-    //vatInput.value = ""
- //   finalCostInput.textContent = "_____________________"
- //   message.textContent = ""
-//}
+   
 
 
 
-
-function handleEvent() {
-    if(Number(unitInput.value) && Number(daysInput.value)) {
-        calculateTotalCost()
-    } else {
-        alert('Give some valid inputs first and try again!');
-        //reset()
-    }
-
-}
-
-
-successButton.addEventListener('click', handleEvent);
-//resetButton.addEventListener('click', reset)
